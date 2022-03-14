c81d1bba885c05f3cf440809188023ec059b0b3f
Philipp Schirmer <philipp.schirmer@bakdata.com>
2022-03-14 09:26:32 +0100

    Create dead letter analyzer

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import net.researchgate.release.ReleaseExtension
 description = "Kafka Streams application that analyzes dead letters in your Kafka cluster"
 
 plugins {
-    `java-library`
+    java
     id("net.researchgate.release") version "2.8.1"
     id("com.bakdata.sonar") version "1.1.7"
     id("com.bakdata.sonatype") version "1.1.7"
@@ -24,6 +24,7 @@ allprojects {
 
     repositories {
         mavenCentral()
+        maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
         maven(url = "https://packages.confluent.io/maven/")
     }
 }
@@ -44,7 +45,7 @@ configure<com.bakdata.gradle.SonatypeSettings> {
 
 configure<org.hildan.github.changelog.plugin.GitHubChangelogExtension> {
     githubUser = "bakdata"
-    githubRepository = "kafka-brute-force-serde"
+    githubRepository = "kafka-dead-letter-analyzer"
     futureVersionTag = findProperty("changelog.releaseVersion")?.toString()
     sinceTag = findProperty("changelog.sinceTag")?.toString()
 }
