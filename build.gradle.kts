abf02cf230f25945d76120196e20d1b381600452
Philipp Schirmer <philipp.schirmer@bakdata.com>
2025-03-12 18:27:10 +0100

    Use improved KStreams

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     val streamsBootstrapVersion = "3.6.2-SNAPSHOT"
     api(platform("com.bakdata.kafka:streams-bootstrap-bom:$streamsBootstrapVersion"))
     api(group = "com.bakdata.kafka", name = "streams-bootstrap-large-messages")
-    implementation(group = "com.bakdata.kafka", name = "streams-bootstrap-cli")
+    api(group = "com.bakdata.kafka", name = "streams-bootstrap-cli")
     implementation(group = "com.bakdata.kafka", name = "brute-force-serde", version = "1.3.0")
     implementation(group = "com.bakdata.kafka", name = "large-message-serde")
     implementation(group = "org.jooq", name = "jool", version = "0.9.15")
